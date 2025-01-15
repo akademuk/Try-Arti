d141008ff1b6e05ef92542717e861ec74d449179
Oleg <of@weblife.ua>
2025-01-15 12:03:10 +0200

    fix

--- script/main.js
+++ script/main.js
@@ -141,3 +141,17 @@ function initModalHandlers() {
 }
 
 
+document.addEventListener('DOMContentLoaded', () => {
+  const togglePassword = document.getElementById('togglePassword');
+  const passwordInput = document.getElementById('password');
+
+  togglePassword.addEventListener('click', () => {
+      const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+      passwordInput.setAttribute('type', type);
+
+      // Меняем иконку
+      const newIcon = type === 'password' ? 'image/eye-closed.svg' : 'image/eye-closed.svg';
+      togglePassword.src = newIcon;
+  });
+});
+
